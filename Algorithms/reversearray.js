60303b8c5a0e7559055b652ad54136a18cb8a88a
Rohan <c1ph3r9@gmail.com>
2018-09-16 18:23:16 -0500

    adding better solution for reverse

--- Algorithms/reversearray.js
+++ Algorithms/reversearray.js
@@ -1,2 +1,13 @@
 
 const revBySort = (array) => array.sort((a, b) => a < b);
+
+//better way
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
